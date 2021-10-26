4c6c300f561689806a20aa87bb0d412233da8a74
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-10-26 17:47:27 -0400

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return(<div>
-      Github Card
-    </div>);
+      ✪
+    </div>
+    );
   }
 }
 
